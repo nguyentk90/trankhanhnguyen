753df41cb169225937006e21c5789e8597145c8a
khanhsnguyen@gmail.com <khanhsnguyen@gmail.com@2250d5e2-00f3-87d0-de18-33c9ea955190>
2013-05-28 10:03:42 +0000

    28-5

--- TraCuuThuatNgu/TraCuuThuatNgu/ChatHub.cs
+++ TraCuuThuatNgu/TraCuuThuatNgu/ChatHub.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using TraCuuThuatNgu.Models;
+using System.Web.Security;
 
 namespace TraCuuThuatNgu
 {
     public class ChatHub:Hub
     {
-        public void Send(string message)
+        QAModel qaModel = new QAModel();
+        MembershipUser currentUser = Membership.GetUser();
+
+        public void Send(string content, string type, int qid)
         {
-            Clients.All.broadcastMessage(message);
+            if (type.Equals("Q"))
+            {                
+                // Add question into database
+                Question question = new Question();
+                question.QContent = content;
+                question.UserId = (Guid)currentUser.ProviderUserKey;
+                question.DateAdd = DateTime.Now;
+                question.DateModify = DateTime.Now;
+                question.Reported = 0;
+
+                string date = String.Format("{0:d/M/yyyy HH:mm}", question.DateAdd);
+
+                int result = qaModel.NewQuestion(question);
+
+                qid = question.QuestionId;
+
+
+                // Display all clients
+                Clients.All.newQuestion(currentUser.UserName,content,type,qid,date);
+            }
+            else
+            { 
+                // Add answer into database
+                Answer answer = new Answer();
+                answer.UserId = (Guid)currentUser.ProviderUserKey;
+                answer.AContent = content;
+                answer.DateAdd = DateTime.Now;
+                answer.Reported = 0;
+                answer.QuestionId = qid;
+                
+                string date = String.Format("{0:d/M/yyyy HH:mm}", answer.DateAdd);
+
+                qaModel.NewAnswer(answer);
+                // Display all clients
+                Clients.All.newAnswer(currentUser.UserName, content, type, qid, date);
+            }
         }
     }
 }
