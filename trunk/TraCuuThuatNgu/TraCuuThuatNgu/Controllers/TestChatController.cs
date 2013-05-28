029dd0445b6e669d85306e706a1fb6b15d1dce49
khanhsnguyen@gmail.com <khanhsnguyen@gmail.com@2250d5e2-00f3-87d0-de18-33c9ea955190>
2013-05-28 10:03:42 +0000

    28-5

--- trunk/TraCuuThuatNgu/TraCuuThuatNgu/Controllers/TestChatController.cs
+++ trunk/TraCuuThuatNgu/TraCuuThuatNgu/Controllers/TestChatController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TraCuuThuatNgu.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
 
 namespace TraCuuThuatNgu.Controllers
 {
@@ -10,11 +13,50 @@ namespace TraCuuThuatNgu.Controllers
     {
         //
         // GET: /TestChat/
-
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(string param)
+        {
+            QAModel qaModel = new QAModel();
+            var list = qaModel.GetQuestionPaged(1, 10).ToList();
+
+            List<QuestionC> question = new List<QuestionC>();
+
+            foreach (var item in list)
+            {
+                QuestionC q = new QuestionC();
+                q.QContent = item.QContent;
+                foreach (var i in item.Answers)
+                {
+                    AnswerC a = new AnswerC();
+                    a.AContent = i.AContent;
+                    a.Qid = i.QuestionId;
+                    q.AList.Add(a);
+                }
+                question.Add(q);
+            }
+            return Json(question, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class QuestionC
+    {
+        public QuestionC()
+        {
+            AList = new List<AnswerC>();
+        }
+        public string QContent { get; set; }
+        public List<AnswerC> AList { get; set; }
+    }
+
+    public class AnswerC
+    {
+        public string AContent { get; set; }
+        public int Qid { get; set; }
     }
 }
